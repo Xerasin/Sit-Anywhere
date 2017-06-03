5fadf041ad39e91fd4ebf3dcf25f9abc5dfa6d62
Xerasin <phoenixteam20@gmail.com>
2017-06-03 16:48:06 -0700

    ahhhhhhhhhhh

--- lua/autorun/client/sit.lua
+++ lua/autorun/client/sit.lua
@@ -5,11 +5,12 @@ local function ShouldAlwaysSit(ply)
 end
 
 hook.Add("KeyPress","seats_use",function(ply,key)
+	if not IsFirstTimePredicted() then return end
+	
 	if key ~= IN_USE then return end
 	
 	local walk=ply:KeyDown(IN_WALK) or ShouldAlwaysSit(ply)
 	if not walk then return end
 	
 	RunConsoleCommand("sit")
-	
 end)
