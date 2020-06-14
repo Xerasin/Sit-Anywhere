15aeb1b0af7ea0f75bceeb2697899d9c4b02a05d
Katherine Loveland <phoenixteam20@gmail.com>
2020-06-14 16:46:32 -0700

    Update Python Again

--- sit_post.py
+++ sit_post.py
@@ -4,18 +4,16 @@ from pprint import pprint
 from subprocess import check_output
 def run():
     dir_path = os.path.dirname(os.path.realpath(__file__))  + "\\"
-
     repository_path = pygit2.discover_repository(dir_path)
     repo = pygit2.Repository(repository_path)
 
-    commitId = ("{}").format(repo.head.target)
-    currentCommit = repo[commitId]
     currentWorkshop = repo.lookup_reference("refs/tags/workshop")
 
-    lastWorkshop = commitId = ("{}").format(currentWorkshop.target)
+    currentCommitID = ("{}").format(repo.head.target)
+    workshopCommitID = ("{}").format(currentWorkshop.target)
 
-    diffUrl = ("https://github.com/Xerasin/Sit-Anywhere/compare/{}..{}").format(lastWorkshop, commitId)
-    if lastWorkshop == commitId:
+    diffUrl = ("https://github.com/Xerasin/Sit-Anywhere/compare/{}..{}").format(workshopCommitID, currentCommitID)
+    if workshopCommitID == currentCommitID:
         print("No changes!")
         return
 
@@ -23,13 +21,16 @@ def run():
 
     out = check_output("\"D:\\Program Files (x86)\\Steam\\SteamApps\\common\\GarrysMod\\bin\\gmad.exe\" create -folder \".\\sit\" -out \".\\Sit.gma\"", shell=True)
 
-    formatStr = """Update to [url=https://github.com/Xerasin/Sit-Anywhere/commit/{0}]{0}[/url]"""
-    changelog = (formatStr).format(commitId, ahhh.message)
+    changelog = ("""Update to [url=https://github.com/Xerasin/Sit-Anywhere/commit/{0}]{0}[/url]""").format(currentCommitID)
 
     if diffUrl != "":
         changelog = ("{} [url={}]Diff[/url]").format(changelog, diffUrl)
     pprint(changelog)
 
-    out = check_output(("\"D:\\Program Files (x86)\\Steam\\SteamApps\\common\\GarrysMod\\bin\\gmpublish.exe\" update -addon \".\Sit.gma\"  -id \"108176967\" -changes \"{}\"").format(changelog), shell=True)
-    repo.create_reference("refs/tags/workshop", commitId)
+    #out = check_output(("\"D:\\Program Files (x86)\\Steam\\SteamApps\\common\\GarrysMod\\bin\\gmpublish.exe\" update -addon \".\Sit.gma\"  -id \"108176967\" -changes \"{}\"").format(changelog), shell=True)
+    
+    repo.references.delete("refs/tags/workshop")
+    repo.create_reference("refs/tags/workshop", currentCommitID)
+
+    repo.push()
 run()
