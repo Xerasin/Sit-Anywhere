3f3cfc8dd5edb61d3ad88b41432c49e964408296
Katherine Loveland <phoenixteam20@gmail.com>
2020-06-14 16:56:51 -0700

    f me

--- sit_post.py
+++ sit_post.py
@@ -28,9 +28,9 @@ def run():
         changelog = ("{} [url={}]Diff[/url]").format(changelog, diffUrl)
     pprint(changelog)
 
-    #out = check_output(("\"D:\\Program Files (x86)\\Steam\\SteamApps\\common\\GarrysMod\\bin\\gmpublish.exe\" update -addon \".\Sit.gma\"  -id \"108176967\" -changes \"{}\"").format(changelog), shell=True)
+    out = check_output(("\"D:\\Program Files (x86)\\Steam\\SteamApps\\common\\GarrysMod\\bin\\gmpublish.exe\" update -addon \".\Sit.gma\"  -id \"108176967\" -changes \"{}\"").format(changelog), shell=True)
     pprint(out)
-
+    
     repo.references.delete("refs/tags/workshop")
     repo.create_reference("refs/tags/workshop", currentCommitID)
 run()
