1cb0ed46dbf7accf8479cac71d33b1f4c210def5
Katherine Loveland <phoenixteam20@gmail.com>
2020-06-14 16:09:42 -0700

    Fix Unstuck

--- sit/lua/autorun/sitanywhere.lua
+++ sit/lua/autorun/sitanywhere.lua
@@ -2,6 +2,7 @@ if SERVER then
     AddCSLuaFile("sitanywhere/client/sit.lua")
     AddCSLuaFile("sitanywhere/ground_sit.lua")
     include("sitanywhere/server/sit.lua")
+    include("sitanywhere/server/unstuck.lua")
     include("sitanywhere/ground_sit.lua")
 else
     include("sitanywhere/client/sit.lua")
