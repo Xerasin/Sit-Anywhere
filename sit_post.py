482ce7c1d1b55a17e52cd60c7b1530177681200f
Katherine Loveland <phoenixteam20@gmail.com>
2020-06-14 16:51:57 -0700

    Ahhh

--- sit_post.py
+++ sit_post.py
@@ -26,14 +26,14 @@ def run():
     pprint(changelog)
 
     while True:
-        query = input('Should I update the Workshop?')
-        Fl = query[0].lower()
+        query = input('Push to Workshop? ')
+        response = query[0].lower()
         if query == '' or not Fl in ['y','n']:
             print('Please answer with yes or no!')
         else:
             break
     
-    if Fl == 'y':
+    if response == 'y':
         out = check_output(("\"D:\\Program Files (x86)\\Steam\\SteamApps\\common\\GarrysMod\\bin\\gmpublish.exe\" update -addon \".\Sit.gma\"  -id \"108176967\" -changes \"{}\"").format(changelog), shell=True)
     
     repo.references.delete("refs/tags/workshop")
