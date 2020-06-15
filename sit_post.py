074e026dafdac362316731d8b488e08c92dfc6db
Katherine Loveland <phoenixteam20@gmail.com>
2020-06-14 17:06:07 -0700

    k

--- sit_post.py
+++ sit_post.py
@@ -4,7 +4,7 @@ from pprint import pprint
 from subprocess import check_output
 
 githubUrl = "https://github.com/Xerasin/Sit-Anywhere"
-
+gmodUtilDir = "D:\\Program Files (x86)\\Steam\\SteamApps\\common\\GarrysMod\\bin"
 def run():
     dir_path = os.path.dirname(os.path.realpath(__file__))  + "\\"
     repository_path = pygit2.discover_repository(dir_path)
@@ -15,22 +15,21 @@ def run():
     currentCommitID = ("{}").format(repo.head.target)
     workshopCommitID = ("{}").format(currentWorkshop.target)
 
-    diffUrl = ("{}/compare/{}..{}").format(githubUrl, workshopCommitID, currentCommitID)
+    
     if workshopCommitID == currentCommitID:
         print("No changes!")
         return
 
-    out = check_output("\"D:\\Program Files (x86)\\Steam\\SteamApps\\common\\GarrysMod\\bin\\gmad.exe\" create -folder \".\\sit\" -out \".\\Sit.gma\"", shell=True)
+    out = check_output(("\"{}\\gmad.exe\" create -folder \".\\sit\" -out \".\\Sit.gma\"").format(gmodUtilDir), shell=True)
 
-    changelog = ("""Update to [url={0}/commit/{1}]{1}[/url]""").format(githubUrl, currentCommitID)
+    diffUrl = ("{}/compare/{}..{}").format(githubUrl, workshopCommitID, currentCommitID)
 
-    if diffUrl != "":
-        changelog = ("{} [url={}]Diff[/url]").format(changelog, diffUrl)
+    changelog = ("""Update to [url={0}/commit/{1}]{1}[/url] 
+    [url={2}]Differences[/url] """).format(githubUrl, currentCommitID, diffUrl)
+
     pprint(changelog)
+    out = check_output(("\"{}\\gmpublish.exe\" update -addon \".\Sit.gma\"  -id \"108176967\" -changes \"{}\"").format(gmodUtilDir, changelog), shell=True)
 
-    out = check_output(("\"D:\\Program Files (x86)\\Steam\\SteamApps\\common\\GarrysMod\\bin\\gmpublish.exe\" update -addon \".\Sit.gma\"  -id \"108176967\" -changes \"{}\"").format(changelog), shell=True)
-    pprint(out)
-    
     repo.references.delete("refs/tags/workshop")
     repo.create_reference("refs/tags/workshop", currentCommitID)
 run()
