0dae99bd5832e962888649c6cd8157f4c325d2a2
Katherine Loveland <phoenixteam20@gmail.com>
2020-06-14 17:01:34 -0700

    Removing old batch

--- sit_post.bat
+++
@@ -1,3 +0,0 @@
-"D:\Program Files (x86)\Steam\SteamApps\common\GarrysMod\bin\gmad.exe" create -folder ".\sit" -out ".\Sit.gma"
-set /p id="ChangeLog: "
-"D:\Program Files (x86)\Steam\SteamApps\common\GarrysMod\bin\gmpublish" update -addon ".\Sit.gma" -id "108176967" -changes "%id%"
