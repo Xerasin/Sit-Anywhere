307ce1f3b494cc12bbdecb5b8136630cc8b2776e
Xerasin <phoenixteam20@gmail.com>
2021-02-01 15:04:33 -0800

    Strip it down!

--- sit/lua/autorun/sitanywhere.lua
+++ sit/lua/autorun/sitanywhere.lua
@@ -3,13 +3,7 @@ if SERVER then
     AddCSLuaFile("sitanywhere/helpers.lua")
     include("sitanywhere/helpers.lua")
     include("sitanywhere/server/sit.lua")
-
-    AddCSLuaFile("sitanywhere/ground_sit.lua")
-    include("sitanywhere/server/unstuck.lua")
-    include("sitanywhere/ground_sit.lua")
 else
     include("sitanywhere/helpers.lua")
     include("sitanywhere/client/sit.lua")
-
-    include("sitanywhere/ground_sit.lua")
 end
